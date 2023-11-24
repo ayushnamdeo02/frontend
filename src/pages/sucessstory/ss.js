7fc5492bd2c7155689aece80ac916151009b81a3
ayushnamdeo02 <ayushnamdeo.an@gmail.com>
2023-11-24 13:44:56 +0530

    success

---
+++ src/pages/sucessstory/ss.js
@@ -0,0 +1,22 @@
+import prashik from '../../assets/images/photo/prashik.png'; 
+import leena from '../../assets/images/photo/leena.png';
+import shivani from '../../assets/images/photo/shivani.png';  
+const testimonialData = [
+    {
+      image: prashik,
+      name: 'Prashik Waghmare',
+      text: '“Joining the Heeds Foundation has been a journey of discovery and fulfillment for me. The organization stands out as an exemplary social services entity, characterized by its cooperative management and a team that’s deeply passionate about serving the community. It’s not just about the services we provide; it’s about making a positive difference in people’s lives. The dedication to excellence is palpable in every project we undertake, ensuring that we meet the diverse needs of those we serve. If you’re seeking an organization that genuinely prioritizes making a difference, the Heeds Foundation is where you should be. It’s been an honor to be part of such a committed team and to witness the profound impact of our work.”'
+    },
+    {
+      image: leena,
+      name: 'Leena Umare',
+      text: '“My experience as a volunteer with the Heeds Foundation can be summed up in a few words: rewarding and fulfilling. Volunteering here isn’t just about what you give; it’s also about what you gain — the joy and satisfaction of contributing to a greater cause. Being part of the Foundation has allowed me to see the positive changes we bring to the community. It’s a reminder of the inherent goodness in giving and being part of something larger than oneself. I’ve learned that every small effort counts and collectively, we can make a significant difference.”'
+    },
+    {
+      image: shivani,
+      name: 'Shivani Dahake',
+      text: '“Working with the Heeds Foundation has been an eye-opening experience for me. The team’s management is impeccable, and their impact on society is profoundly positive. Being part of this team has given me a front-row seat to the tangible changes we bring about in the community. It’s inspiring to see how our collective efforts translate into meaningful improvements in people’s lives. The Foundation’s efficiency and effectiveness in driving social change are commendable. It’s a privilege to be part of an organization that not only aims for but also achieves meaningful and lasting change.”'
+    }
+  ]
+
+export default testimonialData;
