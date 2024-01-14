97630cffdfcd412db570287ba6094697234aecf2
ayushnamdeo02 <ayushnamdeo.an@gmail.com>
2024-01-15 02:52:13 +0530

    changes

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -4,8 +4,6 @@ import missionimgabout from '../../assets/images/missionimgabout.jpg';
 import img1 from '../../assets/images/img1.jpg';
 import './about.css'; 
 import { yourContent } from './constants';
-import teamMembers from './teammembers';
-import core from './core';
 import { Helmet } from 'react-helmet';
 import partners from './partners';
 
@@ -45,9 +43,6 @@ class About extends Component {
 
     this.setState({ names });
 }
-
-
-  
   render() {
     const { visibleNamesCount, names } = this.state;
     const visibleNames = names.slice(0, visibleNamesCount);
@@ -178,65 +173,6 @@ class About extends Component {
         
          <section id='section-who' className='who-section'>
           <h2 className='section-who-heading'>WHO WE ARE</h2>
-          {/* <h4 className='section-who-subheading1'>COMMITTIEE</h4>
-          <div class="line"></div>
-          <div className='team-members'>
-        {teamMembers.map((member, index) => (
-          <div key={index} className='team-member'>
-            <div className='image-card'>
-                <img className="image" src={member.photoUrl} alt={`${member.name}'`}  ></img>
-              </div>
-            <h3 className='member-name'>{member.name}</h3>
-            <p className='member-designation'>{member.designation}</p>
-            <div className="card-content">
-                <ul className="social-links">
-                  <li>
-                    <a href={member.socialLinks.facebook} className="fa fa-facebook" aria-label="Facebook"> </a>
-                  </li>
-                  <li>
-                    <a href={member.socialLinks.twitter} className="fa fa-twitter" aria-label="Twitter"> </a>
-                  </li>
-                  <li>
-                    <a href={member.socialLinks.Instagram} className="fa fa-instagram" aria-label="Instagram"> </a>
-                  </li>
-                  <li>
-                    <a href={member.socialLinks.linkedin} className="fa fa-linkedin" aria-label="LinkedIn"> </a>
-                  </li>
-                </ul>
-              </div>
-          </div>
-        ))}
-      </div>
-      <h4 className='section-who-subheading1'>EXECUTIVE BOARD</h4>
-          <div class="line"></div>
-          <div className='our-team-body'>
-          {core.map((member, index) => (
-            <div key={index} className="our-team">
-              <div className="picture">
-                <img className="img-fluid" src={member.imageURL} alt="Team Member" />
-              </div>
-              <div className="team-content">
-                <h3 className="name">{member.name}</h3>
-                <h4 className="title">{member.title}</h4>
-              </div>
-              <ul className="social">
-                <li>
-                  <a href={member.socialLinks.facebook} className="fa fa-facebook" aria-label="Facebook"> </a>
-                </li>
-                <li>
-                  <a href={member.socialLinks.twitter} className="fa fa-twitter" aria-label="Twitter" > </a>
-                </li>
-                <li>
-                  <a href={member.socialLinks.Instagram} className="fa fa-instagram" aria-label="Instagram"> </a>
-                </li>
-                <li>
-                  <a href={member.socialLinks.linkedin} className="fa fa-linkedin" aria-label="LinkedIn"> </a>
-                </li>
-              </ul>
-            </div> 
-            
-          ))}
-        </div> */}
         <h4 className='section-who-subheading1'>VOLUNTEERS</h4>
           <div class="line"></div>
           <div className='names-grid'>
