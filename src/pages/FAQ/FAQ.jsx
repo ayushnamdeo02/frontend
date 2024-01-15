d1c9538fe36230df6825ea83c35607220e4de389
ayushnamdeo02 <ayushnamdeo.an@gmail.com>
2024-01-15 15:26:06 +0530

    chnages

--- src/pages/FAQ/FAQ.jsx
+++ src/pages/FAQ/FAQ.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './faq.css';
-import faqpdf from './FAQ_Heedsfoundation.pdf'; 
 import { Helmet } from 'react-helmet';
 
 class Faq extends Component {
@@ -11,17 +10,77 @@ class Faq extends Component {
     // Split keywords string into an array and log it
     const keywordsArray = keywords.split(', ');
     console.log(keywordsArray);
-}
+  }
+
   render() {
     return (
       <div className='FAQ-container' data-keywords="education, community, empowerment, NGO, Heeds Foundation">
         <Helmet>
-        <h1><title>FAQs - Heeds Foundation</title></h1>
-                    <meta name="description" content="Find answers to common questions about Heeds Foundation" />
-                    <meta name="keywords" content="Heeds Foundation FAQ, NGO FAQ, NAGPUR TOP NGO" />
-                </Helmet>
-        <div className="FAQ-header">FAQ </div>
-        <iframe title="FAQ" className='pdfset' src={faqpdf}></iframe>
+          <title>FAQs - Heeds Foundation</title>
+          <meta name="description" content="Find answers to common questions about Heeds Foundation and our initiatives in education, healthcare, sustainable development, and humanitarian response." />
+          <meta name="keywords" content="Heeds Foundation FAQ, NGO FAQ, NAGPUR TOP NGO, education, community, empowerment, Heeds Foundation initiatives" />
+        </Helmet>
+        <div className="FAQ-header">Heeds Foundation FAQ </div>
+        <div className="FAQ-body">
+          <ol>
+            <li>
+              <strong>What is the main focus of Heeds Foundation?</strong>
+              <p>We focus on empowering communities through education, healthcare, sustainable development, and humanitarian response.</p>
+            </li>
+            <li>
+              <strong>Is Heeds Foundation a registered non-profit?</strong>
+              <p>Yes, we are a registered non-profit organization dedicated to positive community change.</p>
+            </li>
+            <li>
+              <strong>How can I donate to Heeds Foundation?</strong>
+              <p>Visit our website and click on the “Donate” button. You can choose from various donation options available.</p>
+            </li>
+            <li>
+              <strong>How is my donation used?</strong>
+              <p>Donations are primarily used for our initiatives in education, healthcare, sustainable development, and humanitarian efforts.</p>
+            </li>
+            <li>
+              <strong>Can I volunteer for Heeds Foundation?</strong>
+              <p>Absolutely! Visit our website’s “Join Us” section and fill out the volunteer form.</p>
+            </li>
+            <li>
+              <strong>How can I stay updated with Heeds Foundation’s activities?</strong>
+              <p>Stay connected by following us on our social media channels and subscribing to our newsletter.</p>
+            </li>
+            <li>
+              <strong>Do you collaborate with other organizations or institutions?</strong>
+              <p>Yes, we believe in the power of collaboration and frequently partner with various organizations to maximize our impact.</p>
+            </li>
+            <li>
+              <strong>Where are you located?</strong>
+              <p>Our headquarters is in Nagpur City, but we have initiatives and projects running in various regions.</p>
+            </li>
+            <li>
+              <strong>Can I choose a specific initiative to contribute to?</strong>
+              <p>Yes, when making a donation, you can specify which initiative you’d like your funds to support.</p>
+            </li>
+            <li>
+              <strong>Are there internship opportunities available?</strong>
+              <p>Yes, we offer internships in various domains. Check our website’s “Learning Opportunities” for more details.</p>
+            </li>
+            <li>
+              <strong>Do you organize events or workshops?</strong>
+              <p>Yes, we frequently organize events, campaigns, and workshops. Stay tuned to our website and social media for announcements.</p>
+            </li>
+            <li>
+              <strong>Can I start a Heeds Foundation chapter in my city?</strong>
+              <p>We’re always open to expanding our reach. Contact our team for more information on starting a local chapter.</p>
+            </li>
+            <li>
+              <strong>What impact has Heeds Foundation made so far?</strong>
+              <p>Over the years, we’ve positively impacted thousands of lives through our initiatives. Detailed impact reports are available on our website.</p>
+            </li>
+            <li>
+              <strong>Are there corporate partnership opportunities with Heeds Foundation?</strong>
+              <p>Yes, we collaborate with corporates in various capacities. Please visit the “Partnerships” section on our website.</p>
+            </li>
+          </ol>
+        </div>
       </div>
     );
   }
--- src/pages/termandcondition/termandcondition.jsx
+++ src/pages/termandcondition/termandcondition.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import './TC.css';
-import faqpdf from './Termsandcondition_heeedsfoundation.pdf'; 
+import './TC.scss';
 import { Helmet } from 'react-helmet';
 
 class TC extends Component {
@@ -11,7 +10,8 @@ class TC extends Component {
     // Split keywords string into an array and log it
     const keywordsArray = keywords.split(', ');
     console.log(keywordsArray);
-}
+  }
+
   render() {
     return (
       <div className='TC-container' data-keywords="education, community, empowerment, NGO, Heeds Foundation">
@@ -21,9 +21,33 @@ class TC extends Component {
           <meta name="keywords" content="Heeds Foundation, terms and conditions, policies, guidelines" />
           {/* Additional meta tags as needed */}
         </Helmet>
-        <div className="TC-header">TERMS & CONDITION </div>
-        <iframe title="TC" className='pdfset' src={faqpdf}></iframe>
-
+        <div className="TC-header">TERMS & CONDITIONS</div>
+        <div className="TC-content">
+          <p>
+            These terms and conditions outline the rules and regulations for the use of the Heeds Foundation website, accessible at <a href="https://HeedsFoundation.com">https://HeedsFoundation.com</a>.
+          </p>
+          <p>
+            By accessing this website, we assume you accept these terms and conditions. Do not continue to use Heeds Foundation’s website if you do not agree to take all of the terms and conditions stated on this page.
+          </p>
+          <ul>
+            <li>By using this website you are accepting the terms and conditions. They are effective from your first visit.</li>
+            <li>Heeds Foundation may change these terms and conditions from time to time; your use of the website indicates your acceptance of the terms and conditions at the time of use.</li>
+            <li>Heeds Foundation reserves the right to change the content of the website at any time.</li>
+            <li>Heeds Foundation has taken every precaution to make sure the content of this website is accurate and legally correct at the time of appearance. If you believe the content of any of our pages is inaccurate please contact us, email <a href="mailto:heedsfoundation.ngo@gmail.com">heedsfoundation.ngo@gmail.com</a>.</li>
+            <li>Heeds Foundation accepts no liability for loss or damage, including personal injury, resulting from use of this website. Heeds Foundation makes all reasonable efforts to make sure malware or viruses are not transmitted from this website, however this cannot be guaranteed. We recommend that you safeguard your IT equipment before downloading information and files. Heeds Foundation will not accept liability for damage caused by viruses.</li>
+            <li>When we provide links to other websites it does not mean that we approve of or endorse the views and information contained in the website. We accept no liability for damage caused by malware or viruses on websites that we have linked to.</li>
+            <li>Please request permission before using our logos or other branded material. We reserve the right to require our logo to be removed from websites or printed material if it is not in our interests.</li>
+            <li>Our downloadable documents, brochures and web pages are provided for your use. You may copy or print the information for private purpose including teaching. You may not amend the content or use it for commercial purposes without Heeds Foundation’ written consent.</li>
+            <li>Images and photos used on this website are the property of Heeds Foundation or other Copyright owners. The may not be reproduced or used without Heeds Foundation’ or the copyright owner’s written consent.</li>
+            <li>If there is any conflict between these terms and conditions and rules or specific terms of use relating to specific material then the latter shall prevail.</li>
+          </ul>
+          <p>
+            Contact us
+          </p>
+          <p>
+            If you have any questions at all about our privacy policy, please email us: <a href="mailto:heedsfoundation.ngo@gmail.com">heedsfoundation.ngo@gmail.com</a>
+          </p>
+        </div>
       </div>
     );
   }
