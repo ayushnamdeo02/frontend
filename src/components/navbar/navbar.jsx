ebbae514984556e58fbab044a4cbfd5728ac1212
ayushnamdeo02 <ayushnamdeo.an@gmail.com>
2024-01-18 23:52:01 +0530

    chnages

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../../assets/logobgclear.png';
-import whitelogo from '../../assets/whitelogo.png';
+import logo from '../../assets/logobgclear.webp';
+import whitelogo from '../../assets/whitelogo.webp';
 import './navbar.css';
 
 class Navbar extends Component {
@@ -73,21 +73,6 @@ class Navbar extends Component {
   render() {
     const { isMenuOpen, isScrolled, submenuOpen } = this.state;
     return (
-      <div className='main'> 
-        <div class="header-content">
-          <Link className="dte-code" to='/report'>Reg.No: E-3921 (NGP)</Link>
-            <div className="social-media">
-                Follow us on:
-                <a href="https://facebook.com/Heedsfoundation/" className="facebook"><i className="fa fa-facebook"></i></a>
-                <a href="https://instagram.com/heedsfoundation?igshid=ZDc4ODBmNjlmNQ==" className="instagram"><i className="fa fa-instagram"></i></a>
-                <a href="https://www.linkedin.com/company/heeds-foundation/" className="linkedin"><i className="fa fa-linkedin"></i></a>
-                <a href="https://youtube.com/heedsfoundation" className="youtube"><i className="fa fa-youtube"></i></a>
-            </div>
-            <div class="erp-info">
-                <a  href="tel:8668353016"> 📞 +91 8668353016  </a>
-                <a  href="mailto:heedsfoundation.ngo@gmail.com"> ✉️ heedsfoundation.ngo@gmail.com </a> 
-            </div>
-        </div>
         <nav className={`navbar-container ${isMenuOpen ? 'mobile-menu-open' : ''} ${isScrolled ? 'scrolled' : ''}`} onMouseEnter={this.handleLogoMouseEnter} onMouseLeave={this.handleLogoMouseLeave}>
           <div className="navbar-left">
             <Link className="navbar-brand" to="/">
@@ -165,7 +150,6 @@ class Navbar extends Component {
           </ul>
         </div>
       </nav>
-      </div>
     );
   }
 }
