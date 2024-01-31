a75b92b318c5f09c6dbda33a624456f31de11184
ayushnamdeo02 <ayushnamdeo.an@gmail.com>
2024-01-31 20:35:00 +0530

    changes

--- src/pages/about/partners.js
+++ src/pages/about/partners.js
@@ -1,5 +1,6 @@
 import vairagi from '../../assets/images/partners/vairagi.webp';
 import janheet from '../../assets/images/partners/janheet.webp';
+import ztransform from '../../assets/images/partners/ztransform.jpeg';
 const partners = [
   {
     name: 'VAIRAGI LEARNING',
@@ -11,7 +12,11 @@ const partners = [
     designation: 'MEDICAL PARTNER',
     photoUrl: janheet, // Use the imported image directly
   },
-  
+  {
+    name: 'ZNMZ MEDIA PVT.LTD.',
+    designation: 'DIGITAL MEDIA PARTNER',
+    photoUrl: ztransform, // Use the imported image directly
+  },
 ];
 
 export default partners;
