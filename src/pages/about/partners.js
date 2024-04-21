d417cdf417a208892bb0a4b777aca14b48ca5ce0
ayushnamdeo02 <ayushnamdeo.an@gmail.com>
2024-04-21 23:13:09 +0530

    g

--- src/pages/about/partners.js
+++ src/pages/about/partners.js
@@ -1,6 +1,8 @@
 import vairagi from '../../assets/images/partners/vairagi.webp';
 import janheet from '../../assets/images/partners/janheet.webp';
-import ztransform from '../../assets/images/partners/ztransform.jpeg';
+import vairagit from '../../assets/images/partners/vairagitraders.png';
+import fluxion from '../../assets/images/partners/fluxion.png';
+import heybranding from '../../assets/images/partners/heybranding.png';
 const partners = [
   {
     name: 'VAIRAGI LEARNING',
@@ -13,9 +15,19 @@ const partners = [
     photoUrl: janheet, // Use the imported image directly
   },
   {
-    name: 'ZNMZ MEDIA PVT.LTD.',
-    designation: 'DIGITAL MEDIA PARTNER',
-    photoUrl: ztransform, // Use the imported image directly
+    name: ' VAIRAGI TRADERS',
+    designation: 'RETAIL PARTNER',
+    photoUrl: vairagit, // Use the imported image directly
+  },
+  {
+    name: ' FLUXION INNOVATION',
+    designation: 'TECH PARTNER',
+    photoUrl: fluxion, // Use the imported image directly
+  },
+  {
+    name: ' HEY BRANDING MEDIA',
+    designation: ' MEDIA PARTNER',
+    photoUrl: heybranding, // Use the imported image directly
   },
 ];
 
